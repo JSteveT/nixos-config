9fd23012f4a9d90db11599fa98886a4a72626076
James Taylor <james.steve.taylor@gmail.com>
2025-05-03 22:43:29 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "James' flake-based NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.james = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jamest = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
